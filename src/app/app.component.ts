10a94a3d20a8f203ada2111a1a9da6731da4233e
Vivek Rana <ranavivek2567@SEASIAINFOTECH.COM>
2017-09-22 14:52:10 +0530

    Add data to array of objects in template using observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
-import { ReverseFilterPipe } from './app-filter.pipe';
-import { SearchFilterPipe } from './app-searchfilter.pipe';
+import {Component} from '@angular/core';
+import {SearchFilterPipe} from './app-searchfilter.pipe';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html'
 })
 
-export class AppComponent  {
+export class AppComponent {
   public name: string;
   public searchText: string;
   public list: any;
-  public data: any;
-  constructor(private  reverseFilterPipe: ReverseFilterPipe, private  searchFilterPipe: SearchFilterPipe) {
+  public timer: any;
+  public subscription: any;
+
+  constructor(private  searchFilterPipe: SearchFilterPipe) {
     this.searchText = '';
     this.name = 'Test Assignment';
     this.list = [{
@@ -33,14 +35,31 @@ export class AppComponent  {
         name: 'XYZ',
         joining_date: '28/10/2015',
         age: 21
-      }
-    ];
-    let array = this.list;
-    this.data = [];
-    for (let i = 0; i < array.length; i++) {
-      let element = array[i];
-      this.data.push(element);
-    }
-    Array.prototype.push.apply(this.list, this.data);
+      }];
+
+
+    this.timer = Observable.timer(1000 * 10, 1000 * 20);
+
+    this.subscription = this.timer.subscribe(t => {
+      this.appendElements()
+      this.setFormData();
+    });
   }
+
+  private appendElements() {
+    let arrayLength = Object.keys(this.list).length;
+    let oldElement = this.list[arrayLength - 4];
+    let element = Object.assign({}, oldElement);
+    element.age = element.age.toString().split('').reverse().join('');
+    this.list.push(element);
+  }
+
+  private setFormData() {
+    let arrayLength = Object.keys(this.list).length;
+    if (arrayLength > 7) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
-import { ReverseFilterPipe} from './app-filter.pipe';
 import { SearchFilterPipe} from './app-searchfilter.pipe';
+import { OrderBy} from './app-order.pipe';
+import { Format} from './app.datetime';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule],
-  declarations: [ AppComponent, ReverseFilterPipe, SearchFilterPipe ],
+  declarations: [ AppComponent, SearchFilterPipe, OrderBy , Format],
   bootstrap:    [ AppComponent ],
-  providers: [ReverseFilterPipe, SearchFilterPipe]
+  providers: [SearchFilterPipe, OrderBy , Format]
 })
 export class AppModule { }
